68a58c063728dfe703df57a943293b30d3320706
Bivis-s <66497536+Bivis-s@users.noreply.github.com>
2020-10-27 21:24:35 +0300

    start 9th homework

---
+++ lesson9/src/by/bivis/shop/Shop.java
@@ -0,0 +1,12 @@
+package by.bivis.shop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Shop {
+    private List<Item> itemsList = new ArrayList<>();
+
+//    public boolean addItemToList(Item item) {
+//    }
+
+}
